d8a7c698427b016884533b7e4652f3743a93849e
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-02-04 10:54:26 -0600

    chore: improve endpoint for creating order

--- src/controller/create-order-controller.ts
+++ src/controller/create-order-controller.ts
@@ -2,43 +2,74 @@ import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 
 import getAuthToken from "../auth/get-auth-token";
 import createOrder from "../order/create-order";
+import products from "../data/products.json";
 
 import type { CreateOrderRequestBody } from "@paypal/paypal-js";
 
-const productDatabase = {
-  "PRODUCT-123": {
-    price: 50,
-    status: "IN_STOCK",
-  },
-  "PRODUCT-456": {
-    price: 100,
-    status: "IN_STOCK",
-  },
+type CartItem = {
+  sku: "PRODUCT_123" | "PRODUCT_456";
+  quantity: number;
 };
 
+function getTotalAmount(cartItems: CartItem[]): string {
+  let amountValue = 0;
+
+  cartItems.forEach(({ sku, quantity }) => {
+    if (!products[sku]) {
+      throw new Error(`INVALID_PRODUCT_SKU: ${sku}`);
+    }
+    amountValue += parseFloat(products[sku].price) * quantity;
+  });
+
+  return amountValue.toLocaleString("en-US", { minimumFractionDigits: 2 });
+}
+
+async function createOrderHandler(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const { cart } = request.body as { cart: CartItem[] };
+  const { access_token: accessToken } = await getAuthToken();
+
+  const orderPayload: CreateOrderRequestBody = {
+    intent: "CAPTURE",
+    purchase_units: [
+      {
+        amount: {
+          currency_code: "USD",
+          value: getTotalAmount(cart),
+        },
+      },
+    ],
+  };
+
+  const data = await createOrder(accessToken, orderPayload);
+  reply.send(data);
+}
+
 export default async function createOrderController(fastify: FastifyInstance) {
-  fastify.post(
-    "/create-order",
-    async function (request: FastifyRequest, reply: FastifyReply) {
-      const { access_token: accessToken } = await getAuthToken();
-
-      // TODO: use this data to calculate the order amount
-      console.log({ body: request.body });
-
-      const orderPayload: CreateOrderRequestBody = {
-        intent: "CAPTURE",
-        purchase_units: [
-          {
-            amount: {
-              currency_code: "USD",
-              value: "100.00",
+  fastify.route({
+    method: "POST",
+    url: "/create-order",
+    handler: createOrderHandler,
+    schema: {
+      body: {
+        type: "object",
+        required: ["cart"],
+        properties: {
+          cart: {
+            type: "array",
+            items: {
+              type: "object",
+              required: ["sku", "quantity"],
+              properties: {
+                sku: { type: "string" },
+                quantity: { type: "number" },
+              },
             },
           },
-        ],
-      };
-
-      const data = await createOrder(accessToken, orderPayload);
-      reply.send(data);
-    }
-  );
+        },
+      },
+    },
+  });
 }
