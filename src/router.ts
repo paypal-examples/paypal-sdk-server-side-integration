b4adf80a07e7fd86161675239ad54881ed81dbee
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-02-09 09:54:26 -0600

    chore: add global error handler for providing more details in the json response

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,10 @@ import type { FastifyInstance } from "fastify";
 import { clientTokenController } from "./controller/client-token-controller";
 import { createOrderController } from "./controller/order-controller";
 import { configController } from "./controller/config-controller";
+import { setErrorHandler } from "./controller/error-controller";
 
 export default async function router(fastify: FastifyInstance) {
+  setErrorHandler(fastify);
   fastify.register(clientTokenController, { prefix: "/api/paypal" });
   fastify.register(createOrderController, { prefix: "/api/paypal" });
   fastify.register(configController, { prefix: "/api/paypal" });
