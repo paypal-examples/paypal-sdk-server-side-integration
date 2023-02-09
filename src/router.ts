2f55dd2e48c42273d111e3d55e1485e6de3451c5
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-02-09 09:53:14 -0600

    chore: update config controller naming

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import { clientTokenController } from "./controller/client-token-controller";
 import { createOrderController } from "./controller/order-controller";
-import { clientIDController } from "./controller/config-controller";
+import { configController } from "./controller/config-controller";
 
 export default async function router(fastify: FastifyInstance) {
   fastify.register(clientTokenController, { prefix: "/api/paypal" });
   fastify.register(createOrderController, { prefix: "/api/paypal" });
-  fastify.register(clientIDController, { prefix: "/api/paypal" });
+  fastify.register(configController, { prefix: "/api/paypal" });
 }
