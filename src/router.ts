9f35baf8c0a434afde43ef3bcba88cdbee1d386a
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-02-09 14:39:12 -0600

    chore: add basic functionality for order capture

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import type { FastifyInstance } from "fastify";
 import { clientTokenController } from "./controller/client-token-controller";
-import { createOrderController } from "./controller/order-controller";
+import {
+  createOrderController,
+  captureOrderController,
+} from "./controller/order-controller";
 import { configController } from "./controller/config-controller";
 import { setErrorHandler } from "./controller/error-controller";
 
@@ -8,5 +11,6 @@ export default async function router(fastify: FastifyInstance) {
   setErrorHandler(fastify);
   fastify.register(clientTokenController, { prefix: "/api/paypal" });
   fastify.register(createOrderController, { prefix: "/api/paypal" });
+  fastify.register(captureOrderController, { prefix: "/api/paypal" });
   fastify.register(configController, { prefix: "/api/paypal" });
 }
