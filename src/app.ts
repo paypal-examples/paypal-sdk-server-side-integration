0388c907453947dd608ef3451322d7655618787d
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-03-06 16:55:30 -0600

    chore: add pino-pretty for formatting logs in dev

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,25 @@ import path from "path";
 
 import router from "./router";
 
+const envToLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
+
+const environment =
+  (process.env.NODE_ENV as keyof typeof envToLogger) ?? "development";
+
 const server = fastify({
-  // Logger used for production only
-  logger: process.env.NODE_ENV !== "development",
+  logger: envToLogger[environment] ?? true,
 });
 
 server.register(router);
