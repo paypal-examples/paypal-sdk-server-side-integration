1d1c63fbb0b28f5347ec68bc73733e2096c8d2c0
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-03-17 11:39:26 -0500

    chore: replace require with import for fastify static

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify, { FastifyServerOptions } from "fastify";
+import fastifyStatic from "@fastify/static";
 import path from "path";
 
 import router from "./router";
@@ -8,7 +9,7 @@ export default function buildApp(options: FastifyServerOptions = {}) {
 
   server.register(router);
 
-  server.register(require("@fastify/static"), {
+  server.register(fastifyStatic, {
     root: path.join(__dirname, "../", "public"),
   });
 
